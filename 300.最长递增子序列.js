3a432d3a1901438a44a2163f33e08b0dc83ec2af
Kylin Lee <lee.kylin@outlook.com>
2021-03-21 21:21:14 +0800

    add(300): 新增一个问题，未解决

---
+++ 300.最长递增子序列.js
@@ -0,0 +1,119 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ *
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (48.33%)
+ * Likes:    1455
+ * Dislikes: 0
+ * Total Accepted:    230.1K
+ * Total Submissions: 476K
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7]
+ * 的子序列。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [0,1,0,3,2,3]
+ * 输出：4
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [7,7,7,7,7,7,7]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * -10^4
+ *
+ *
+ *
+ *
+ * 进阶：
+ *
+ *
+ * 你可以设计时间复杂度为 O(n^2) 的解决方案吗？
+ * 你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  var record = []
+  // 最大值
+  var res = 1
+  nums.forEach((num, index) => {
+    let tempIndex, temp
+    // 选择的数组、差值
+    // 循环创建的每个数组
+    for (let index = 0; index < record.length; index++) {
+      const t = num - record[index][0]
+      if (temp === undefined) {
+        temp = t
+      }
+      if (t > 0 && t < temp) {
+        tempIndex = index
+      }
+      temp = t
+    }
+    if (tempIndex !== undefined) {
+      record[tempIndex].unshift(num)
+      res = record[tempIndex].length > res ? record[tempIndex].length : res
+    }
+
+    // 没有该元素作为起始则新建数组
+    if (!record[index]) {
+      record[index] = [num]
+    }
+  })
+  return res
+}
+// @lc code=end
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const lengthOfLIS = function (nums) {
+  const res = 1
+
+  const iterationArr = []
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index]
+  }
+
+  return res
+}
+// @lc code=end
+
+module.exports = lengthOfLIS
---
+++ index.js
@@ -0,0 +1,3 @@
+const lengthOfLIS = require("./300.最长递增子序列")
+
+lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])
